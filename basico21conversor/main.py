3d3edf930f4a06cb005a331a6a2e058b47756ad6
JesusDiazCurso <59970683+JesusDiazCurso@users.noreply.github.com>
2020-03-30 12:00:16 +0200

    Update main.py

--- basico21conversor/main.py
+++ basico21conversor/main.py
@@ -2,13 +2,6 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from ventanas import ventana_principal
 import sys
 
-#tareas sobre el conversor:
-# 1- Crear una nueva aplicacion que permita la conversion de euros a un tipo de moneda
-#    a elegir por el programador
-# 2- Sobre dicha aplicacion, agregar dos botones mas para poder transformar a otros dos 
-#    tipos de moneda
-# 3- Buscar en internet una solucion para poder 
-#    agregar un icono a cada boton representando el tipo de moneda
 
 def convertir_de_pesos_a_euros():
     print("boton pulsado")
