c795ba73e0cbf57d7055c1c42b30dfdef005e665
JesusDiazCurso <59970683+JesusDiazCurso@users.noreply.github.com>
2020-04-10 12:11:23 +0200

    Add files via upload

---
+++ tareascursoLunes/modelo/clases.py
@@ -0,0 +1,8 @@
+class Juego():
+
+    def __init__(self):
+        self.tipodejuego = ""
+        self.nombrejuego = ""
+        self.plataforma = ""
+        self.añosalida = 0
+        self.precio = 0.0
