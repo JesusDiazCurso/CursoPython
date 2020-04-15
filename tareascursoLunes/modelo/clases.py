a5191bacd4e94d09e5c4d10c15d6962dba590939
JesusDiazCurso <59970683+JesusDiazCurso@users.noreply.github.com>
2020-04-15 13:40:20 +0200

    Delete clases.py

--- tareascursoLunes/modelo/clases.py
+++
@@ -1,8 +0,0 @@
-class Juego():
-
-    def __init__(self):
-        self.tipodejuego = ""
-        self.nombrejuego = ""
-        self.plataforma = ""
-        self.añosalida = 0
-        self.precio = 0.0
