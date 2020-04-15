fa942cc07263f70a3670b6d9f16592a397df927e
JesusDiazCurso <59970683+JesusDiazCurso@users.noreply.github.com>
2020-04-15 13:40:26 +0200

    Delete constantes_sql.py

--- tareascursoLunes/modelo/constantes_sql.py
+++
@@ -1,3 +0,0 @@
-SQL_INSERCION_JUEGO = "INSERT INTO `tabla_juegos` (`id`, `Tipo de Juego`, `Nombre de juego`, `Plataforma`, `Año de Salida`, `Precio`) VALUES (NULL, %s, %s, %s, %s, %s);"
-SQL_SELECT_JUEGOS = "SELECT * FROM tabla_juegos" 
-SQL_BORRAR_JUEGO = "DELETE FROM tabla_juegos WHERE id = %s ;"
